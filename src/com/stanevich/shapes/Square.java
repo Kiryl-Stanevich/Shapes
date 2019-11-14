f82c9dc64b333affb2186c283a69e58904431d2e
Kiryl-Stanevich <56796225+Kiryl-Stanevich@users.noreply.github.com>
2019-11-14 22:41:34 +0300

    #6 Code Review (Add class Square)

---
+++ src/com/stanevich/shapes/Square.java
@@ -0,0 +1,16 @@
+package com.stanevich.shapes;
+
+public class Square {
+    Point p1 = new Point();
+    Point p2 = new Point();
+    Point p3 = new Point();
+    Point p4 = new Point();
+    public void getCenter ()
+    {
+
+    }
+    public void Draw ()
+    {
+
+    }
+}
