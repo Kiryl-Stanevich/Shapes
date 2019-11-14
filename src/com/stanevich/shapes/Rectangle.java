75af3af73e6bed789de3cebe1019e3d1772c77b7
Kiryl-Stanevich <56796225+Kiryl-Stanevich@users.noreply.github.com>
2019-11-14 22:38:53 +0300

    #4 Code Review (Add class Rectangle)

---
+++ src/com/stanevich/shapes/Rectangle.java
@@ -0,0 +1,17 @@
+package com.stanevich.shapes;
+
+public class Rectangle {
+    Point p1 = new Point();
+    Point p2 = new Point();
+    Point p3 = new Point();
+    Point p4 = new Point();
+    public void getCenter ()
+    {
+
+    }
+    public void Draw ()
+    {
+
+    }
+}
+
