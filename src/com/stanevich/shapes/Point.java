f214a866df30ab3ece3fa14d62a53f02b2e1d5aa
Kiryl-Stanevich <56796225+Kiryl-Stanevich@users.noreply.github.com>
2019-11-18 23:06:17 +0300

    #7 Code Review (Add constructor in the class Point)

--- src/com/stanevich/shapes/Point.java
+++ src/com/stanevich/shapes/Point.java
@@ -2,4 +2,13 @@ package com.stanevich.shapes;
 
 public class Point {
     public int x,y;
+    public Point (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public Point ()
+    {
+
+    }
 }
